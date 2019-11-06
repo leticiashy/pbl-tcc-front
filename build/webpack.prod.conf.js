30a51f1bd5286285b1afea744e141b5d68ccc275
João Marcus Fernandes <joao.fernandes@zooxcloud.com>
2019-11-06 19:04:15 -0300

    Fixed version

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -64,8 +64,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename:
-        process.env.NODE_ENV === "testing" ? "index.html" : config.build.index,
+      filename: "index.html",
       template: "index.html",
       inject: true,
       minify: {
